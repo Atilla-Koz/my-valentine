eaf5d8362c4f234d9e445ac401ca7407229ef950
Atilla-Koz <atillakoz@hotmail.com>
2024-06-25 19:52:42 +0300

    chore: Update react-router-dom to version 5.2.0

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Valentine from './Components/Valentine';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/">
+          <Valentine />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Valentine.js
@@ -0,0 +1,13 @@
+export default function Valentine() {
+  return (
+    <section className="flex flex-col items-center gap-16">
+      <div>
+        <div>
+          <h1>Will You Be My Valentine ?</h1>
+        </div>
+        <button>Click me</button>
+        <button>Click me</button>
+      </div>
+    </section>
+  );
+}
